6ab4a98cf7a818c4841d126af0378281f1301b9b
gracekeywork <156124211+gracekeywork@users.noreply.github.com>
2026-02-16 14:40:03 -0500

    Initial Commit

---
+++ Music2/Music2App.swift
@@ -0,0 +1,17 @@
+//
+//  Music2App.swift
+//  Music2
+//
+//  Created by grace keywork on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Music2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
