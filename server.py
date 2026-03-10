fa20a2135092eb165a73bb1d966a74d743a0dec9
lucasrn17 <lrn15@pitt.edu>
2026-03-10 12:03:46 -0400

    more complete functionality
    -return lyrics now works
    -slightly higher file organization
    -testing is still a bit of a pain; next step is to make file navigation and testing easier by adding more Song class parameters (filepaths)

--- server.py
+++ server.py
@@ -3,15 +3,19 @@ from pydantic import BaseModel
 from fastapi.responses import FileResponse
 from audio_separator.separator import Separator
 import os
+import shutil
+import whisper
 
 server = FastAPI()
+
+model = whisper.load_model("base")
 
 class Song():
     def __init__(self, name, artist, filename):
         self.name = name
         self.artist = artist
         self.filename = filename
-        self.lyrics = ""
+        self.lyric_file = ""
 
 class Song_request(BaseModel):
     name : str
@@ -37,48 +41,85 @@ async def upload_song(file: UploadFile):
     #add new song to list
     song_list.append(new_song)
 
-    #create directory SHOULD PROBABLY ADD FILEEXISTSERROR EXCEPTION HERE
-    os.mkdir(new_song.name)
+    #create directory
+    try: os.mkdir(new_song.name)
+    except FileExistsError: print("File already exists")
 
     #save the .wav file
     with open(file.filename, "wb") as f:
         contents = await file.read()
         f.write(contents)
 
-        #run song separation before returning true
-        separate_song(new_song)
+        #run song separation
+        #separate_song(new_song)
 
-        return True
+        #run lyric extraction
+        #extract_lyrics(new_song)
+
+    #before returning true, move all files into its 'Song name' directory
+    shutil.move(new_song.name+" - "+new_song.artist+".wav", new_song.name)
+    shutil.move(new_song.name+" - "+new_song.artist+"_vocals.wav", new_song.name)
+    shutil.move(new_song.name+" - "+new_song.artist+"_drums.wav", new_song.name)
+    shutil.move(new_song.name+" - "+new_song.artist+"_bass.wav", new_song.name)
+    shutil.move(new_song.name+" - "+new_song.artist+"_guitar.wav", new_song.name)
+    shutil.move(new_song.name+" - "+new_song.artist+"_lyrics.txt", new_song.name)
+
+    #save filepath to lyrics in song object
+    new_song.lyric_file = new_song.name+"/"+new_song.name+" - "+new_song.artist+"_lyrics.txt"
+    print(new_song.lyric_file)
+
+    return True
 
 @server.get("/requests/{song_title}/{instrument_type}")
 async def return_song(song_title: str, instrument_type: str):
     return_file = ''
-    for item in song_list:
-        if item.name == song_title:
-            song_file = item.filename
-            index = song_file.find('.')
-            return_file = song_file[:index] + instrument_type +'.wav'
+    ####song_list.append(Song("Rooster", "Alice in Chains", "Rooster - Alice in Chains.wav")) #JUST FOR TESTING
+    for song in song_list:
+        if song.name == song_title:
+            return_file = song.name+"/"+song.name+" - "+song.artist+"_"+instrument_type+".wav"
             return FileResponse(return_file)
     if return_file == '':
         return "File DNE"
 
-@server.get("requests/{song_title}_lyrics")
+@server.get("/requests/{song_title}_lyrics")
 def return_lyrics(song_title: str):
-    return song_title.lyrics
+    for song in song_list:
+        if song.name == song_title:
+            return FileResponse(song.lyric_file)
+        
+
+
+
+
+########HELPER FUNCTIONS
 
 def separate_song(song_obj):
     separator = Separator()
     separator.load_model('htdemucs.yaml')
     #names the output files in the format song name - artist_instrument.wav
     output_names = {
-        "Vocals": song_obj.name+" - "+song_obj.artist+"_vocals.wav",
-        "Drums": song_obj.name+" - "+song_obj.artist+"_drums.wav",
-        "Bass": song_obj.name+" - "+song_obj.artist+"_bass.wav",
-        "Guitar": song_obj.name+" - "+song_obj.artist+"_guitar.wav"
+        "Vocals": song_obj.name+" - "+song_obj.artist+"_vocals",
+        "Drums": song_obj.name+" - "+song_obj.artist+"_drums",
+        "Bass": song_obj.name+" - "+song_obj.artist+"_bass",
+        "Other": song_obj.name+" - "+song_obj.artist+"_guitar"
     }
 
     separator.output_dir = song_obj.name
+    print(separator.output_dir)
     separator.separate(song_obj.filename, output_names)
+
+def extract_lyrics(song_obj):
+    result = model.transcribe(song_obj.name+" - "+song_obj.artist+"_vocals.wav", fp16=False) #disable 16 bit floating point so it uses 32 on cpu
+    lyric_text = ''
+    for c in result["text"]:
+        if c == '.':
+            lyric_text += '\n'
+        else:
+            lyric_text += c
+
+    #create lyric file and write to it
+    with open(song_obj.name+" - "+song_obj.artist+"_lyrics.txt", "w") as f:
+        f.write(lyric_text)
 
 #uvicorn server:server --reload <-- private launch
 #uvicorn server:server --host 10.5.22.60 --port 8000 --reload
